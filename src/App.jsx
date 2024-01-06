6f4d7a0348863c060fdc38d22e54877e8311ff2f
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2024-01-06 13:55:37 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from './components/Buttons'
+
+function App() {
+
+
+  return (
+    <div>
+      WELCOME TO MY APP
+      <div className="p-5">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+
+const Buttons = () => {
+
+  return (
+    <div>
+      <button  className="px-4 py-2 bg-slate-600 text-white font-bold rounded-md hover:bg-slate-800">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default Buttons
